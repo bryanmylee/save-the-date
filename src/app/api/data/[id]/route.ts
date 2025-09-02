04616e491b1e89a7a95dee583c2ef1e8543d7da2
Bryan Lee <bryanmylee@gmail.com>
2025-09-02 21:19:37 +0800

    data: update names

--- src/app/api/data/[id]/route.ts
+++ src/app/api/data/[id]/route.ts
@@ -3,18 +3,18 @@ import { NextResponse } from "next/server";
 const nameForId: Record<string, string> = {
   "643625": "Mom & Dad",
   "003296": "Benjamin, my best man",
-  "520784": "Ho Juin Jie and Marianne Terng",
+  "520784": "Ho Juin Jie & Marianne Terng",
   "425504": "Brennan Sze To",
   "940125": "Yeo Yong Kang",
-  "263905": "Emmanuel Oh and Joyce Chng",
-  "895245": "Ma",
-  "184195": "Derrick Ng & Andrea Ng|Alexandria Ng & Annabeth Ng",
-  "201940": "Bernard Ong and Stephanie Goh",
+  "263905": "Emmanuel Oh & Joyce Chng",
+  "895245": "Helena Ng",
+  "184195": "Derrick Ng , Andrea Boo ,|Alexandria Ng , Annabeth Ng",
+  "201940": "Bernard Ong & Stephanie Goh",
   "154526": "James Koh & Namuunaa Nadmid",
   "597813": "Ng Yen",
   "410513": "Vaisnavi Subramaniam",
   "395710": "Bernice Tan",
-  "232878": "Mike",
+  "232878": "Athena Tam ",
 };
 
 type Params = {
