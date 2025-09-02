180812e5e7ad0bfab1aef2dfde40389957ee629e
Bryan Lee <bryanmylee@gmail.com>
2025-09-02 22:34:26 +0800

    feat: low power mode fallback

--- src/app/[id]/video-background.tsx
+++ src/app/[id]/video-background.tsx
@@ -1,11 +1,32 @@
 "use client";
 
+import Image from "next/image";
+import { useState } from "react";
+
 export function VideoBackground() {
+  const [isLowPower, setLowPower] = useState(false);
+
+  if (isLowPower) {
+    return (
+      <Image
+        src="/waves.webp"
+        alt="Background wave image"
+        width={800}
+        height={1061}
+        aria-hidden
+        className="absolute inset-0 object-cover w-screen h-screen opacity-40"
+      />
+    );
+  }
+
   return (
     <video
       ref={(node) => {
         if (node != null) {
           node.playbackRate = 0.5;
+          if (node.currentTime === 0 && (node.paused || node.ended)) {
+            setLowPower(true);
+          }
         }
       }}
       src="/waves.webm"
